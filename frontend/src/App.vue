695ad795eca7b74f746227edb0f85c6000f389b4
Coquetic <arno.coquet35@gmail.com>
2025-09-01 11:53:04 +0200

    Create App.vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <RouterView />
+</template>
+
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<style scoped>
+body {
+  margin: 0;
+  padding: 0;
+}
+
+.logo {
+  height: 50px;
+}
+
+nav a {
+  margin: 0 1rem;
+  text-decoration: none;
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
